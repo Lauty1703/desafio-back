3ac8a2d8ce2f3faf9241e645b1fe9fc9a3dd0895
Lautaro fernandez <lautyfz1703@gmail.com>
2022-10-22 01:17:26 -0300

    subida de desafio 2

--- script.js
+++ script.js
@@ -1,160 +1,5 @@
-// const mostrart=(mostrar)=>{
-//     console.log(mostrar)
-// }
-// mostrart("q onda gil")
 
-// const saludar=(nombre)=> {
-//     return `hola ${nombre}`
-// }
-// const mensaje=saludar("lautaro");
-// console.log(mensaje)
-
-// const calculadora=(num1,num2,operacion)=>{
-//     return operacion(num1,num2)
-// }
-// const sumar=(a,b)=>a+b;
-// const suma=(calculadora(10,10,sumar))
-// console.log(suma)
-
-
-
-// const restar=(a,b)=>a-b;
-// const resta=calculadora(10,10,restar)
-// console.log(resta)
-
-
-// const multiplicar=(a,b)=>a*b;
-// const multiplica=calculadora(10,10,multiplicar)
-// console.log(multiplica)
-
-
-// const dividir=(a,b)=>a/b;
-// const dividi=calculadora(10,10,dividir)
-// console.log(dividi)
-
-// const  multiplicar=(num1,num2)=>{
-//     if(typeof num1 !== 'number' || typeof num2 != 'number'){
-//         return Promise.reject("los nu")
-//     }
-//     return new Promise((resolve, reject) => {
-
-
-//     setTimeout(()=>{
-
-//         resolve({
-//             num1,
-//             num2,
-//             result:num1*num2
-//         })
-//     },Math.floor(Math.random()*2000))})
-// }
-
-// multiplicar(15,10)    
-//     .then(response=>{
-//         console.log(`${response.num1} * ${response.num2} = ${response.result }`)
-//          return multiplicar(2,2)
-//     })
-//     .then(response=>{
-//         console.log(`${response.num1} * ${response.num2} = ${response.result }`)
-//     })
-
-// let tiempo=5
-
-// const timer=setInterval(() => {
-//     console.log(`quedan ${tiempo}`)
-//     tiempo++
-
-//     if(tiempo===10){
-//         clearInterval(timer);
-//     }
-
-// }, 1000);
-
-// const delay=(ret)=>{
-//     for (let i = 0; i <ret*3e6; i++);
-// };
-
-
-// function hacerTarea(num){
-//     console.log(`hacer tarea `+ num)
-//     delay(1000)
-// }
-
-// console.log("inicio de tarea");
-// hacerTarea(1)
-// hacerTarea(2)
-// hacerTarea(3)
-// hacerTarea(4)
-// console.log("fin de tarea")
-// console.log("otras tareas...")
-
-// function mostrarLetras(palabra,fin) {
-//     let i = 0
-//     const timer = setInterval(() => {
-//         if (i < palabra.length) {
-//             console.log(palabra[i])
-//             i++
-
-//         }else{
-//             clearInterval(timer)
-//             fin()
-
-//         }
-//     }, 1000);
-
-// }
-
-// const fin=()=>console.log("termine");
-
-// setTimeout(() => {
-//     mostrarLetras(`hola`, fin)
-// },0 );
-
-// setTimeout(() => {
-//     mostrarLetras(`hola`, fin)
-// },250 );
-
-// setTimeout(() => {
-//     mostrarLetras(`hola`, fin)
-// },250 );
 const fs = require('fs')
-
-// const datos=fs.readFileSync('datos.txt','utf-8');
-// console.log(datos)
-
-// fs.writeFileSync('datos.txt','que hace compañero','utf-8')
-
-// fs.appendFileSync('datos.txt',' \ncompañerito','utf-8')
-// try{
-// fs.unlinkSync('datos.txt',' \ncompañerito','utf-8')
-// }
-// catch(err){
-// console.log(err)
-// }
-// console.log('fin del programa')
-
-// fs.readFile('datos.txt','utf-8',(error,dato)=>{
-//     if(error){
-//         console.log(error)
-//     }else{
-//         console.log(dato)
-//     }
-// })
-
-// fs.readFile('package.json', 'utf-8', (error,contenido)=>{
-//     if(error){throw new error('erro al leer el archivo')}
-//         console.log(contenido)
-
-//         const info={
-//             contenido:contenido,
-//             contenidoObjeto:JSON.parse(contenido),
-//             size:contenido.length
-//         }
-//         fs.writeFile('info.txt',JSON.stringify(info),(error)=>{
-//             if(error){throw new error('erro al leer el archivo')}
-//         })
-
-// })
 
 class Contenedor {
     constructor(nameArchivo) {
@@ -216,25 +61,27 @@ class Contenedor {
     }
 }
 
-const newArchivo = new Contenedor("productos.txt");
 
-let objeto=[{
+objeto = [{
+    id: 0,
+    nombre: "rey",
+    thumbnail:"https://i.ibb.co/S541325/oferta6.jpg"
+},
+{
     id: 1,
-    nombre: 'helado de crema de oreo ',
-    imagen: './img/oferta1.jpg',
-}, {
-    id: 2,
-    nombre: ' cofler dulce de leche ',
-    precio: 2000,
-    imagen: './img/oferta2.jpg',
+    nombre: "rey",
+    thumbnail:"https://i.ibb.co/S541325/oferta2.jpg"
 },{
-    id: 3,
-    nombre: 'helado de frutilla y crema',
-    imagen: './img/oferta3.jpg',
-
-}]
-
-newArchivo.save(objeto)
-
+    id: 2,
+    nombre : "rey",
+    thumbnail:"https://i.ibb.co/S541325/oferta5.jpg"
+}
+]
+const newArchivo = new Contenedor("./productos.txt");
 
 
+newArchivo.save(objeto).then(resolve => console.log(resolve));
+newArchivo.getById(1).then(resolve => console.log(resolve));
+newArchivo.getAll().then(resolve => console.log(resolve));
+newArchivo.deleteById(10);
+newArchivo.deleteAll();
