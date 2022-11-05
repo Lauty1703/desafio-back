05d23fd1ca8538a7224f4662bb7c4957dd013195
Lautaro fernandez <lautyfz1703@gmail.com>
2022-11-04 23:51:42 -0300

    incorporando plantillas

--- script.js
+++ script.js
@@ -1,112 +1,91 @@
 
-const fs = require('fs')
 
-class Contenedor {
-    constructor(nameArchivo) {
-        this.nameArc = nameArchivo
-    }
-
-    async save(object) {
-        let auxArray = []
-        try {
-            const data = await fs.promises.readFile(this.nameArc, "utf-8")
-            auxArray = JSON.parse(data)
-            let idArray = auxArray.map(obj => obj.id)
-            let highId = Math.max(...idArray)
-            object.id = highId + 1;
-            auxArray.push(object);
-            fs.writeFileSync(this.nameArc, JSON.stringify(auxArray))
-        }
-        catch {
-            object.id = 0;
-            auxArray.push(object);
-            fs.writeFileSync(this.nameArc, JSON.stringify(auxArray))
-        }
-        return object.id
-    }
-    async getById(number) {
-        try {
-            const data = await fs.promises.readFile(this.nameArc, "utf-8")
-            let auxArray = JSON.parse(data)
-            const object = auxArray.find(obj => obj.id === number)
-            return object
-        }
-        catch {
-            return null
-        }
-    }
-    async getAll() {
-        try {
-            const data = await fs.promises.readFile(this.nameArc, "utf-8")
-            const auxArray = JSON.parse(data)
-            return auxArray
-        }
-        catch {
-            return null
-        }
-    }
-    async deleteById(number) {
-        try {
-            const data = await fs.promises.readFile(this.nameArc, "utf-8")
-            const auxArray = JSON.parse(data)
-            const newArray = auxArray.filter(obj => obj.id !== number)
-            fs.writeFileSync(this.nameArc, JSON.stringify(newArray))
-        }
-        catch {
-            return "No hay objetos en el archivo"
-        }
-    }
-    deleteAll() {
-        fs.writeFileSync(this.nameArc, "")
+class Contenedor{
+    constructor (){
+        this.productos=[];
     }
 
-    async getRandom() {
-        try {
-            let products = await this.getAll();
-            let productsJSON = JSON.parse(products)
-            let random = Math.floor(Math.random() * productsJSON.length)
-            return productsJSON[random]
-        } catch (error) {
-            return []
-        }
+    newId=1;
+
+    getAll(){
+        return this.productos;
     }
 
+    getProdId(id){
+        let oneProd= this.productos.filter(p=> p.id== Number(id))
+        return oneProd;
+    }
+
+    postProd(prod){
+        prod.id = this.newId ++;
+
+        this.productos.push(prod);
+        
+    }
 }
 
 
+const products= new Contenedor();
 
-const newArchivo = new Contenedor("./productos.txt");
+const express = require('express');
+const app = express();
+const path = require('path');
+const router=express.Router();
 
+const port = 8080 || process.env.PORT;
 
-// newArchivo.save(objeto).then(resolve => console.log(resolve));
-// newArchivo.getById(1).then(resolve => console.log(resolve));
-// newArchivo.getAll().then(resolve => console.log(resolve));
-// newArchivo.deleteById(10);
-// newArchivo.deleteAll();
+app.set('views', './views');
+app.set('view engine', 'ejs')
 
-console.log(newArchivo);
-
-
-
-const express = require("express");
-app = express();
-const PORT = 8080;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-app.get('/productos', (req, res) => {
-    newArchivo.getAll().then(resolve => { res.end(`productos : ${JSON.stringify(resolve)}`) });
 
-
+// agregar producto
+router.get('/agregarProducto', (req, res)=>{
+    res.render('addproduct')
 })
 
-app.get('/productoRandom', (req, res) => {
-    let random = parseInt((Math.random() * 3) + 1)
-    newArchivo.getById(random)
-        .then((resolve) => {
-            res.send(`producto random: ${JSON.stringify(resolve)}`)
-        })
+// mostrar productos
+router.get('/mostrarProductos', (req, res)=>{
+    const data = products.getAll();
+    res.render('productos', {productos:data})
 })
 
+
+router.post('/', (req,res)=>{
+    let datos = products.postProd(req.body);
+    res.redirect('/productos/agregarProducto')
+
+})
+
+router.get('/detalle/:id', (req,res) =>{
+    let oneProd= products.getProdId(req.params.id);
+    if(oneProd.length == 0){
+        res.send('Producto no encontrado')
+    }else{
+        res.render('productos', {productos: oneProd});
+    }
+})
+
+
+
+app.use('/productos', router);
+
+const server = app.listen(port,()=>{
+    console.log(`se esta escuchando en el puerto ${port}`)
+})
+
+server.on('error', err=>(console.log(err)));
+
+
+
+
+// let products=[
+//     {
+//         title:"Computadora",
+//         price: "130.000",
+//         thumbnail: "https://www.tecnologia-informatica.com/wp-content/uploads/2018/07/funciones-de-la-computadora-1.jpeg"
+//     }
+// ]
